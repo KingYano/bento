3ecacd565d5139fd7a59ea253fc9a3d95418c458
KingYano <belkadi.yanis.l@gmail.com>
2024-01-14 23:46:05 +0100

    finalisation du composant singleinfo

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
-
-<main>
   <p>Hello World</p>
-</main>
+  <SingleInfo
+    :imageUrl="'./src/assets/logo.svg'"
+    :text="'Yanis Belkadi'"
+    :buttonText="'Se connecter'"
+  ></SingleInfo>
 
 </template>
 
 <script setup lang="ts">
-
+  import SingleInfo from "./components/Card/SingleInfo/SingleInfo.vue";
 </script>
 
 <style lang="scss">
---
+++ src/components/Card/SingleInfo/SingleInfo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="single-card">
+    <div class="single-card__icon">
+      <img class="single-card__icon-image" :src="imageUrl" :alt="altText">
+    </div>
+    <div class="single-card__title">
+      <p class="single-card__title-text">{{ text }}</p>
+    </div>
+    <div class="single-card__command">
+      <button class="single-card__command-button">{{ buttonText }}</button>
+    </div>
+  </div>
+</template>
+
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  imageUrl: String,
+  altText: String,
+  text: String,
+  buttonText: String
+});
+</script>
+
+<style>
+  @import './SingleInfo.scss';
+</style>
