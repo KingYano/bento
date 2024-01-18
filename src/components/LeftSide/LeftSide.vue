92a66e45a9910dc72db2a591202c60f33935ab01
KingYano <belkadi.yanis.l@gmail.com>
2024-01-18 18:00:50 +0100

    ajout classe et modification

--- src/components/LeftSide/LeftSide.vue
+++ src/components/LeftSide/LeftSide.vue
@@ -5,12 +5,12 @@
     </div>
     <div class="left-side-paragraph">
       <h1 class="left-side-paragraph-title">Yanis BELKADI</h1>
-      <p>Je suis Yanis, développeur web front-end et UX/UI designer vivant à Paris, spécialisé en VueJS et Javascript.</p>
-      <p>Actuellement en dernière année d’étude (+5) en alternance  au sein d'une entreprise dynamique, développant un SaaS applicatif innovant. au sein d'une entreprise dynamique, développant un SaaS applicatif innovant.</p>
-      <p>Prestations :</p>
-      <p>-> Landing page / Site vitrine, totalement responsive en Html/Css/Js.</p>
-      <p>-> Webapps en Vue.Js.</p>
-      <p>-> Fonctionnalités en Vue.Js.</p>
+      <p class="left-side-paragraph-introduction">Je suis Yanis, développeur web front-end et UX/UI designer vivant à Paris, spécialisé en VueJS et Javascript.</p>
+      <p class="left-side-paragraph-details">Actuellement en dernière année d’étude (+5) en alternance  au sein d'une entreprise dynamique, développant un SaaS applicatif innovant. au sein d'une entreprise dynamique, développant un SaaS applicatif innovant.</p>
+      <p class="left-side-paragraph-heading">Prestations :</p>
+      <p class="left-side-paragraph-services">-> Landing page / Site vitrine, totalement responsive en Html/Css/Js.</p>
+      <p class="left-side-paragraph-services">-> Webapps en Vue.Js.</p>
+      <p class="left-side-paragraph-services">-> Fonctionnalités en Vue.Js.</p>
     </div>
 
     <BadgeWork
@@ -19,7 +19,8 @@
     ></BadgeWork>
 
     <div class="left-side-copyright">
-      <p>2024 - ©Yanis Belkadi V◦1</p>
+      <p>©Yanis Belkadi - 2024</p>
+      <p>V◦1</p>
     </div>
 
   </section>
