1a29cec75af1ecb429d5baa27a1bf8717f4361da
KingYano <belkadi.yanis.l@gmail.com>
2024-01-18 17:44:39 +0100

    Creation du component leftside avec implémentation du code

---
+++ src/components/BadgeWork/BadgeWork.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="badge-work" :class="isOpen ? 'badge-work-open' : 'badge-work-not-open'">
+    <p class="badge-work-message">{{ status }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps({
+  isOpen: Boolean,
+  status: String,
+});
+</script>
+
+<style lang="scss">
+  @import 'BadgeWork.scss';
+</style>
---
+++ src/components/LeftSide/LeftSide.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="left-side-content">
+    <div class="left-side-image">
+      <img class="left-side-image-profile" src="@/assets/images/image_profile.webp">
+    </div>
+    <div class="left-side-paragraph">
+      <h1 class="left-side-paragraph-title">Yanis BELKADI</h1>
+      <p>Je suis Yanis, développeur web front-end et UX/UI designer vivant à Paris, spécialisé en VueJS et Javascript.</p>
+      <p>Actuellement en dernière année d’étude (+5) en alternance  au sein d'une entreprise dynamique, développant un SaaS applicatif innovant. au sein d'une entreprise dynamique, développant un SaaS applicatif innovant.</p>
+      <p>Prestations :</p>
+      <p>-> Landing page / Site vitrine, totalement responsive en Html/Css/Js.</p>
+      <p>-> Webapps en Vue.Js.</p>
+      <p>-> Fonctionnalités en Vue.Js.</p>
+    </div>
+
+    <BadgeWork
+      :isOpen="false"
+      status="Open to Work"
+    ></BadgeWork>
+
+    <div class="left-side-copyright">
+      <p>2024 - ©Yanis Belkadi V◦1</p>
+    </div>
+
+  </section>
+</template>
+
+<script setup lang="ts">
+
+import BadgeWork from "@/components/BadgeWork/BadgeWork.vue";
+</script>
+
+<style lang="scss">
+@import 'LeftSide.scss';
+</style>
